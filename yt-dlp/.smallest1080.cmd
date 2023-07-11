7bc8b066e74ee5a880c393058495995f8f767fac
Rinsuo <ilvessuo24@gmail.com>
2023-07-11 12:22:15 +0300

    format + sorting remade

--- yt-dlp/.smallest1080.cmd
+++ yt-dlp/.smallest1080.cmd
@@ -2,7 +2,7 @@
 setlocal EnableDelayedExpansion
 :start
 set /p link= Enter link: 
-..\yt-dlp.exe -S "res:1080,abr,+size" !link! --no-abort-on-error --wait-for-video 10
+..\yt-dlp.exe -f "bv[height=1080]+ba[abr<100]" -S "abr,+size" !link! --no-abort-on-error --wait-for-video 10 --concurrent-fragments 4
 
 echo finished
 goto start
